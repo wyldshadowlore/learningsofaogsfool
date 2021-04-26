bc6be1f1911bafe0cc09cd872d3da903e70f0354
WyldLore Collab <83235587+wyldshadowlore@users.noreply.github.com>
2021-04-26 13:54:22 -0500

    Update somethingtopractice.py

--- somethingtopractice.py
+++ somethingtopractice.py
@@ -11,4 +11,14 @@ def attack():  ## Initialize Function
     
 attack()
 
-#
+#Objects
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
