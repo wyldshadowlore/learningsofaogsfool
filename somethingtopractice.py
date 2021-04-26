22efd3e46e67f746ba021b75f325f041be1f1984
WyldLore Collab <83235587+wyldshadowlore@users.noreply.github.com>
2021-04-26 12:57:43 -0500

    Create somethingtopractice.py

---
+++ somethingtopractice.py
@@ -0,0 +1,14 @@
+##Copy one section at a time to practice
+
+# The basics of a function
+
+import random
+
+def attack():
+    print("Dude attacks: ")
+    damage = random.randrange(1, 10, 1)
+    print("Attacked for ", damage, " damage.")
+    
+attack()
+
+#
